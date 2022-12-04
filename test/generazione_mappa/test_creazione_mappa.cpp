da164941f58a73f6b6b43c7aa4561630b0657bab
Alessio Lovato <alessio.lovato.1@studenti.unipd.it>
2022-12-04 19:28:36 +0100

    Testo aggiornato con la nuova configurazione della libreria mappa

--- test/generazione_mappa/test_creazione_mappa.cpp
+++ test/generazione_mappa/test_creazione_mappa.cpp
@@ -17,68 +17,44 @@ void stampa_gnuplot(mappa &map) {
     //ciclo stampa valori
     // 0 -> no ostacoli
     // 1 -> ostacoli
-    posizione stampa{map.posizione_minima().first , map.posizione_minima().second};
-    while (stampa.second <= map.posizione_massima().second) {
-        while(stampa.first <= map.posizione_massima().first) {
-          file << stampa.first << " " << stampa.second << " ";
-             if (map.ostacolo_in_posizione(stampa) == true)
-                file << "1";
-            else
-                file << "0";
-            stampa.first++;
-            file << endl;
+    for (float x{map.posizione_minima().first + (map.dimensione_celle_metri()/2)}; x < map.posizione_massima().first; x += map.dimensione_celle_metri()) {
+        for(float y = map.posizione_minima().second + (map.dimensione_celle_metri()/2); y < map.posizione_massima().second; y += map.dimensione_celle_metri()) {
+          file << x << " " << y << " ";
+          file << map.ostacolo_in_posizione(posizione{x,y});
+          file << endl;
         }
         file << endl;
-        stampa.second++;
-        stampa.first = map.posizione_minima().first;
     }
     file.close();
 
     //stampa grafico
     stampa_grafico(map.posizione_minima().first,map.posizione_minima().second,
-                  map.posizione_massima().first, map.posizione_massima().second);
+                  map.posizione_massima().first, map.posizione_massima().second, map.dimensione_celle_metri());
   
 }
 
 
 int main(int argc, char *argv[]) {
-  if (argc == 1 || argc >3) {
+  if (argc <4 || argc >4) {
     cerr << "Deve essere inserito il percorso del file da cui ricavare gli ostacoli e nient'altro." << endl;
     exit(EXIT_FAILURE);
   }
 
-  std::ifstream file_conteggio(argv[1], std::ios::in);
-  if (!file_conteggio.is_open()) {
-    std::cerr << "Errore nell'apertura di: " << argv[1] << endl;
-    exit(EXIT_FAILURE);
-  } else {
- 
-    //numero di ostacoli presenti nel file
-    int righe{conta_ostacoli_da_file(file_conteggio)};
-    file_conteggio.close();
-
-    //vettore contenente le posizioni degli ostacoli
-    vector<ostacolo> ostacoli;
-    ostacoli.reserve(righe);
-    crea_vettore_ostacoli(argv[1], ostacoli);
-
     //mappa
-    mappa griglia{ostacoli};
+    mappa griglia{argv[1], std::stof(argv[3])};
     griglia.stampa_ostacoli();
 
     //stampo il vettore di ostacoli
-    stampa_vettore_ostacoli(ostacoli);
+    //stampa_vettore_ostacoli(ostacoli);
 
     //stampa dimensioni griglia
-    cout << "Minimo grigia--> x: " << griglia.posizione_minima().first << "  y: " << griglia.posizione_minima().second << endl
-    << "Massimo grigia--> x: " << griglia.posizione_massima().first << "  y: " << griglia.posizione_massima().second << endl;
+    cout << "Minimo griglia--> x: " << griglia.posizione_minima().first << "  y: " << griglia.posizione_minima().second << endl
+    << "Massimo griglia--> x: " << griglia.posizione_massima().first << "  y: " << griglia.posizione_massima().second << endl;
 
     //stampa griglia
     griglia.stampa_mappa(argv[2]);
 
     //inizio stampa gnuplot
     stampa_gnuplot(griglia);
-
-  }
     return 0;
 }

c127e740f96cbb157cc0a94655a0fdd07fd668f8
Alessio Lovato <alessio.lovato.1@studenti.unipd.it>
2022-12-08 00:49:12 +0100

    Implementata la prima versione della pianificazione del moto. Ora l'algoritmo sarà testato per trovare eventuali criticità

--- lib/robot/include/robot.h
+++ lib/robot/include/robot.h
@@ -3,15 +3,24 @@
 
 #include "mappa.h"
 #include <iostream>
+#include <queue>
 
-using std::pair;
-typedef pair<float, float> posizione; // corrisponderà a (x,y)
+using std::queue;
+typedef float distanza;
+//In dati_cella: first == distanza, second == potenziale
+typedef pair<distanza, float> dati_cella;
 
 
 class Robot
 {
 public:
     Robot(const posizione &robot, const posizione &obbiettivo, mappa &mappa_riferimento, const float raggio_robot = 0.5);
+    void raggiungi_obbiettivo(const posizione &obbiettivo);
+    void aggiorna_mappa(mappa &mappa_riferimento) const {mappa_riferimento.aggiorna_mappa(mappa_);};
+    void sposta_su_cella_successiva(mappa &mappa_condivisa);
+    posizione posizione_attuale() const {return robot_celle_;};
+    posizione obbiettivo() const {return obbiettivo_celle_;};
+
 private:
     posizione robot_celle_;
     posizione robot_reale_;
@@ -19,10 +28,18 @@ private:
     posizione obbiettivo_reale_;
     mappa mappa_;
     float raggio_;
+    queue<posizione> posizioni_precedenti;
 
     void incrementa_mappa(const posizione &posizione_necessaria);
-    void centra_posizione(float &pos);
+    void centra_posizione(distanza &pos);
     void inserisci_dati_robot_su_mappa(posizione &pos, string oggetto);
+    float campo_attrattivo(const posizione &partenza, const posizione &arrivo) const;
+    distanza calcolo_distanza(const posizione &partenza, const posizione &arrivo) const;
+    pair<posizione, distanza> oggetto_piu_vicino(const posizione &attuale, const set<posizione>::const_iterator &inizio_set,
+                                             const set<posizione>::const_iterator &fine_set);
+    float campo_repulsivo(const posizione &partenza, const posizione &arrivo) const;
+    map<posizione, dati_cella> calcola_potenziali_celle_adiacenti();
+    float ricalcolo_potenziale_cella (const posizione &cella_da_ricalcolare, const posizione &robot_ostacolo) const;
+    void aggiorna_campi_potenziale(map<posizione, dati_cella> &celle_con_potenziali);
 };
-
 #endif
--- lib/robot/src/robot.cpp
+++ lib/robot/src/robot.cpp
@@ -6,9 +6,10 @@ Robot::Robot(const posizione &robot, const posizione &obbiettivo, mappa &mappa_r
         inserisci_dati_robot_su_mappa(robot_celle_, "Il robot");
         inserisci_dati_robot_su_mappa(obbiettivo_celle_, "L'obbiettivo");      
         
-        mappa_.rendi_cella_ostacolo(robot_celle_);
-        mappa_.rendi_cella_ostacolo(obbiettivo_celle_);
-        mappa_riferimento.aggiorna_mappa(mappa_);
+        mappa_.posiziona_robot_cella(robot_celle_);
+
+        //attendo mutex per aggiornare mappa
+        aggiorna_mappa(mappa_riferimento);
 
 }
 
@@ -33,7 +34,7 @@ void Robot::incrementa_mappa(const posizione &posizione_necessaria) {
         mappa_.inserisci_celle(minima, massima);
 }
 
-void Robot::centra_posizione(float &pos) {
+void Robot::centra_posizione(distanza &pos) {
         pos /= mappa_.dimensione_celle_metri();
         pos = arrotonda_valore(pos, (pos <= 0.0) ? true : false);
         pos *= mappa_.dimensione_celle_metri();
@@ -54,5 +55,103 @@ void Robot::inserisci_dati_robot_su_mappa(posizione &pos, string oggetto) {
         //incremento in modo che la posizione del robot sia al centro di una cella
         pos.first += (((pos.first <= 0.0) ? (mappa_.dimensione_mezza_cella()) : (-mappa_.dimensione_mezza_cella())));
         pos.second += (((pos.second <= 0.0) ? (mappa_.dimensione_mezza_cella()) : (-mappa_.dimensione_mezza_cella())));       
+
+}
+
+float Robot::campo_attrattivo(const posizione &partenza, const posizione &arrivo) const {
+        return ((mappa_.fattore_scala_campo_attrattivo() * pow(calcolo_distanza(partenza, arrivo), 2.0f))/2.0f);
+}
+
+distanza Robot::calcolo_distanza(const posizione &partenza, const posizione &arrivo) const {
+        return (sqrt(pow((arrivo.first - partenza.first), 2.0f) + pow((arrivo.second - partenza.second), 2.0f)));
+}
+
+pair<posizione, distanza> Robot::oggetto_piu_vicino(const posizione &attuale, const set<posizione>::const_iterator &inizio_set, const set<posizione>::const_iterator &fine_set) {
+        map<posizione, float> distanza_ostacolo;
+        std::for_each(inizio_set, fine_set, [this, &attuale, &distanza_ostacolo](auto pos) {distanza_ostacolo[pos] = calcolo_distanza(attuale, pos);});
+        auto elemento_minimo {min_element(distanza_ostacolo.cbegin(), distanza_ostacolo.cend(), [](auto &lhs, auto &rhs) { return lhs.second < rhs.second;})};
+        return pair{elemento_minimo->first, elemento_minimo->second};
+}
+
+float Robot::campo_repulsivo(const posizione &partenza, const posizione &arrivo) const {
+        distanza dist{calcolo_distanza(partenza, arrivo)};
+        if (dist < mappa_.distanza_minima_ostacolo())
+                return ((mappa_.fattore_scala_campo_repulsivo()*pow(((1/dist)+(1/mappa_.distanza_minima_ostacolo())),2.0f))/2.0f);
+        else
+                return 0.0;
+}
+
+map<posizione, dati_cella> Robot::calcola_potenziali_celle_adiacenti () {
+        map<posizione, dati_cella> potenziali;
+        posizione attuale{robot_celle_};
+        //cancello la mia posizione dagli ostacoli
+        mappa_.libera_cella_robot(robot_celle_);
+        pair<posizione, distanza> ostacolo{oggetto_piu_vicino(attuale, mappa_.ostacoli_cbegin(), mappa_.ostacoli_cend())};
+        //cella +/
+        attuale.first += mappa_.dimensione_celle_metri();
+        if (mappa_.contiene_cella(attuale))
+                potenziali.insert({attuale, {ostacolo.second, campo_attrattivo(attuale,obbiettivo_celle_) + campo_repulsivo(attuale, ostacolo.first)}});
+        //cella ++
+        attuale.second += mappa_.dimensione_celle_metri();
+        if (mappa_.contiene_cella(attuale))
+                potenziali.insert({attuale, {ostacolo.second, campo_attrattivo(attuale,obbiettivo_celle_) + campo_repulsivo(attuale, ostacolo.first)}});
+        //cella /+
+        attuale.first -= mappa_.dimensione_celle_metri();
+        if (mappa_.contiene_cella(attuale))
+                potenziali.insert({attuale, {ostacolo.second, campo_attrattivo(attuale,obbiettivo_celle_) + campo_repulsivo(attuale, ostacolo.first)}});
+        //cella -+
+        attuale.first -= mappa_.dimensione_celle_metri();
+        if (mappa_.contiene_cella(attuale))
+                potenziali.insert({attuale, {ostacolo.second, campo_attrattivo(attuale,obbiettivo_celle_) + campo_repulsivo(attuale, ostacolo.first)}});
+        //cella /-
+        attuale.second -= mappa_.dimensione_celle_metri();
+        if (mappa_.contiene_cella(attuale))
+                potenziali.insert({attuale, {ostacolo.second, campo_attrattivo(attuale,obbiettivo_celle_) + campo_repulsivo(attuale, ostacolo.first)}});
+        //cella --
+        attuale.second -= mappa_.dimensione_celle_metri();
+        if (mappa_.contiene_cella(attuale))
+                potenziali.insert({attuale, {ostacolo.second, campo_attrattivo(attuale,obbiettivo_celle_) + campo_repulsivo(attuale, ostacolo.first)}});
+        //cella /-
+        attuale.first += mappa_.dimensione_celle_metri();
+        if (mappa_.contiene_cella(attuale))
+                potenziali.insert({attuale, {ostacolo.second, campo_attrattivo(attuale,obbiettivo_celle_) + campo_repulsivo(attuale, ostacolo.first)}});
+        //cella +-
+        attuale.first += mappa_.dimensione_celle_metri();
+        if (mappa_.contiene_cella(attuale))
+                potenziali.insert({attuale, {ostacolo.second, campo_attrattivo(attuale,obbiettivo_celle_) + campo_repulsivo(attuale, ostacolo.first)}});
+
+        return potenziali;
+}
+
+float Robot::ricalcolo_potenziale_cella (const posizione &cella_da_ricalcolare, const posizione &robot_ostacolo) const {
+        return (campo_attrattivo(cella_da_ricalcolare, robot_ostacolo) + campo_repulsivo(cella_da_ricalcolare, robot_ostacolo));
+}
+
+void Robot::aggiorna_campi_potenziale (map<posizione, dati_cella> &celle_con_potenziali) {
+        for (auto elemento : celle_con_potenziali) {
+                pair<posizione, distanza> ostacolo{oggetto_piu_vicino(elemento.first, mappa_.robot_cbegin(), mappa_.robot_cend())};
+                if (elemento.first.second > ostacolo.second) {
+                        //associo la nuova distanza alla cella
+                        elemento.second.first = ostacolo.second;
+                        elemento.second.second = ricalcolo_potenziale_cella(elemento.first, ostacolo.first);
+                }
+        }
+}
+
+
+void Robot::sposta_su_cella_successiva(mappa &mappa_condivisa){
+        //prima faccio i calcoli sulla mappa che conosco, poi aggiorno le posizioni dei robot
+        map<posizione, dati_cella> potenziali_celle{calcola_potenziali_celle_adiacenti()};
+        
+        //attendo mutex per leggere posizioni robot aggiornate
+        mappa_.aggiorna_mappa(mappa_condivisa);
+        //aggiorna_campi_potenziale(potenziali_celle);
+        posizione prossima_cella{std::min_element(potenziali_celle.cbegin(), potenziali_celle.cend(),
+                                                        [](auto &lhs, auto &rhs) { return lhs.second.second < rhs.second.second;})->first};
+        mappa_.libera_cella_robot(robot_celle_);
+        robot_celle_ = std::make_pair(prossima_cella.first, prossima_cella.second);
+        mappa_.posiziona_robot_cella(prossima_cella);
+        mappa_condivisa.aggiorna_mappa(mappa_);
+        //rilascio chiave mutex
 
 }

1f9abe1e61ac8838b6690a5036973627cfcee964
Alessio Lovato <alessio.lovato.1@studenti.unipd.it>
2022-12-08 00:50:24 +0100

    Modificato per poter testare pianificazione moto

--- test/generazione_mappa/test_aggiornamento_robot.cpp
+++ test/generazione_mappa/test_aggiornamento_robot.cpp
@@ -18,10 +18,14 @@ void stampa_gnuplot(mappa &map) {
     //ciclo stampa valori
     // 0 -> no ostacoli
     // 1 -> ostacoli
+    // 2 -> robot
     for (float x{map.posizione_minima().first + (map.dimensione_celle_metri()/2)}; x < map.posizione_massima().first; x += map.dimensione_celle_metri()) {
         for(float y = map.posizione_minima().second + (map.dimensione_celle_metri()/2); y < map.posizione_massima().second; y += map.dimensione_celle_metri()) {
           file << x << " " << y << " ";
-          file << map.ostacolo_in_posizione(posizione{x,y});
+          if (map.contains_robot(posizione{x,y}))
+            file << "2";
+          else
+            file << map.ostacolo_in_posizione(posizione{x,y});
           file << endl;
         }
         file << endl;
@@ -66,20 +70,30 @@ int main(int argc, char *argv[]) {
 
     //mappa
     mappa griglia{argv[1], std::stof(argv[3])};
-    griglia.stampa_ostacoli();
+    //griglia.stampa_ostacoli();
 
     //stampa dimensioni griglia
     cout << "Minimo griglia--> x: " << griglia.posizione_minima().first << "  y: " << griglia.posizione_minima().second << endl
     << "Massimo griglia--> x: " << griglia.posizione_massima().first << "  y: " << griglia.posizione_massima().second << endl;
 
     //stampa griglia
-    griglia.stampa_mappa(argv[2]);
+    //griglia.stampa_mappa(argv[2]);
 
     //inizio stampa gnuplot
-    stampa_gnuplot(griglia);
+    //stampa_gnuplot(griglia);
 
-    Robot robot1{posizione{-5,-6}, posizione{20,20}, griglia};
+    Robot robot1{posizione{-5,-6}, posizione{15,15}, griglia};
+  
+    griglia.stampa_mappa(argv[2]);
     stampa_gnuplot(griglia);
+    while ((robot1.posizione_attuale().first != robot1.obbiettivo().first) || (robot1.posizione_attuale().second != robot1.obbiettivo().second)) {
+      robot1.sposta_su_cella_successiva(griglia);
+      griglia.posiziona_robot_cella(posizione{15,15});
+      cout << "posizione: " <<robot1.posizione_attuale().first << " : " << robot1.posizione_attuale().second << endl;
+      stampa_gnuplot(griglia);
+    }
+    cout << "obbietttivo: " <<robot1.obbiettivo().first << " : " << robot1.obbiettivo().second << endl;
+
 
     return 0;
 }

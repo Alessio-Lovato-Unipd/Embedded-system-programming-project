84d1a760a099e5de32bae374b599b7e0133a072a
Alessio Lovato <alessio.lovato.1@studenti.unipd.it>
2022-12-09 22:22:05 +0100

    Revisione codice e inseriti commenti funzioni

--- lib/robot/include/robot.h
+++ lib/robot/include/robot.h
@@ -1,49 +1,110 @@
 #ifndef ROBOT_H
 #define ROBOT_H
 
-#include "mappa.h"
+#include "Mappa.h"
 #include <iostream>
 
 typedef float distanza;
-//In dati_cella: first == distanza, second == potenziale
+//first == distanza, second == potenziale
 typedef pair<distanza, float> dati_cella;
 
 
-class Robot
-{
+class Robot {
 public:
-    Robot(const posizione &robot, const posizione &obbiettivo, mappa &mappa_riferimento, const float raggio_robot = 0.5f);
-    void aggiorna_mappa(mappa &mappa_riferimento) const {mappa_riferimento.aggiorna_mappa(mappa_);};
-    void sposta_su_cella_successiva(mappa &mappa_condivisa);
+    /*Costruttore classe
+	  Parametri:
+	  	- robot: posizione (x,y) alla quale si trova il robot
+		- obbiettivo: posizione (x,y) alla quale deve arrivare il robot
+		- mappa riferimento: mappa alla quale riferirsi
+		- [facoltativo] raggio_robot: dimensione del robot (essendo cilindrico il raggio)
+	*/
+    Robot(const posizione &robot, const posizione &obbiettivo, Mappa &mappa_riferimento, const float raggio_robot = 0.5f);
+
+	/*Funzione per spostare il robot alla cella circostante con potenziale minore rispetto all'obbiettivo
+	  Parametri:
+	  	- mappa_condivisa: mappa sulla quale fanno riferimento i robot
+	*/
+    void sposta_su_cella_successiva(Mappa &mappa_condivisa);
+
+	/*Funzioni accesso dati*/
+	// Ritorna l'elenco delle celle occupate dal robot
     set<posizione> posizione_attuale() const {return celle_occupate_;};
+	// Ritorna la posizione centrale delle celle occupate deal robot
     posizione posizione_centrale() const {return robot_celle_;};
+	// Ritorna la posizione ce il robot deve raggiungere
     posizione obbiettivo() const {return obbiettivo_celle_;};
+	// Ritorna true se una delle celle occupate dal robot contiene l'obbiettivo, altrimenti false
     bool obbiettivo_raggiunto() const {return celle_occupate_.contains(obbiettivo_celle_);};
+	/* Fine funzioni accesso dati */
 
 private:
-    posizione robot_celle_;
-    posizione robot_reale_;
-    posizione obbiettivo_celle_;
-    posizione obbiettivo_reale_;
-    mappa mappa_;
-    double raggio_;
-    set<posizione> posizioni_precedenti;
-    set<posizione> celle_occupate_;
+    posizione robot_celle_; //Posizione centrale del robot nella mappa
+    posizione obbiettivo_celle_; // Posizione dell'obbiettio nella mappa
+    Mappa mappa_; //copia della mappa condivisa con gli altri robot
+    const double raggio_; //Dimensione del raggio del robot
+    set<posizione> posizioni_precedenti; //Elenco delle posizioni precedentemente percorse dal robot
+    set<posizione> celle_occupate_; //Elenco delle celle occupate dal robot
 
+	/*Funzione per modificare le dimensioni della mappa nel caso la posizione passata per argomento
+	  non fosse già all'interno della mappa. Se le dimensioni vengono modificate, aggiungo celle ai bordi modificati.
+	  Parametri:
+	  	- posizione_necessaria: posizione da inserire nella mappa
+	*/
     void incrementa_mappa(const posizione &posizione_necessaria);
+
+	/*Funzione per il calcolo del campo attrattivo tra due posizioni
+	  Parametri:
+	  	- partenza: prima posizione da cui calcolare il potenziale
+		- arrivo: seconda posizione alla quale è riferito il potenziale
+	*/
     float campo_attrattivo(const posizione &partenza, const posizione &arrivo) const;
+
+	/*Funzione per il calcolo del campo repulsivo tra due posizioni
+	  Parametri:
+	  	- partenza: prima posizione da cui calcolare il potenziale
+		- arrivo: seconda posizione alla quale è riferito il potenziale
+	*/
+	float campo_repulsivo(const posizione &partenza, const posizione &arrivo) const;
+
+	/*Funzione per il calcolo della distanza euclidea tra due posizioni
+	  Parametri:
+	  	- partenza: prima posizione da cui calcolare la distanza
+		- arrivo: seconda posizione alla quale è riferita la distanza
+	*/
     distanza calcolo_distanza(const posizione &partenza, const posizione &arrivo) const;
+
+	/*Funzione per il calcolo dell'oggetto più vicino ad una determinata posizione tra un elenco di posizioni occupate
+	  Parametri:
+	  	- attuale: posizione sulla quale calcolare l'oggetto più vicino
+		- inizio_set: iteratore costante all'inizio del set contenente i punti da analizzare
+		- fine_set: iteratore costante alla fine del set contenente i punti da analizzare
+	*/
     pair<posizione, distanza> oggetto_piu_vicino(const posizione &attuale, const set<posizione>::const_iterator &inizio_set,
-                                             const set<posizione>::const_iterator &fine_set);
-    float campo_repulsivo(const posizione &partenza, const posizione &arrivo) const;
-    map<posizione, dati_cella> calcola_potenziali_celle_adiacenti();
+													const set<posizione>::const_iterator &fine_set);
+	
+	/*Funzione per calcolare il potenziale delle celle adiacenti alla posizione centrale attuale del robot. Il calcolo
+	  viene eseguito solo rispetto agli ostacoli e non ai robot.
+	*/
+    map<posizione, dati_cella> calcola_potenziali_celle_adiacenti() ;
+
+	/*Funzione per ricalcolare il campo di potenziale su una cella rispetto ad un nuovo ostacolo o robot
+	  Parametri:
+	  	- cella da calcolare: posizione della cella sulla quale effettuare il ricalcolo
+		- robot_ostacolo: posizione dell'ostacolo che modifica il potenziale
+	*/
     float ricalcolo_potenziale_cella (const posizione &cella_da_ricalcolare, const posizione &robot_ostacolo) const;
+
+	/*Funzione per ricalcolare il potenziale delle celle adiacenti alla posizione attuale (centrale) considerando i robot
+	  presenti nella mappa. La lista dei robot è aggiornata all'inizio della funzione con la lista della mappa condivisa.
+	  Parametri:
+	  	-celle con potenziali: mappa con le celle calcolate in precedenza tramite la funzione 'calcola_potenziali_celle_adaicenti()'
+	*/
     void aggiorna_campi_potenziale(map<posizione, dati_cella> &celle_con_potenziali);
 
     /*Funzione per verificare che il robot non faccia uno spostamento diagonale se adiacente ad
     un altro robot o ad un ostacolo.
     Parametri:
-    - mappa che rappresenta i possibili spostamenti futuri
+    - Mappa che rappresenta i possibili spostamenti futuri
     */
     void limita_spostamenti(map<posizione, dati_cella> &potenziali_celle);
 
@@ -56,13 +117,12 @@ private:
     */
     set<posizione> celle_adiacenti(const posizione &attuale) const;
 
-    /*Funzione verificare se in una determinata cella di ha collisione con
-      altro robot o ostacolo
+    /*Funzione che restituisce true se in una determinata cella si ha collisione con
+      altro robot o ostacolo, altrimenti false
     Parametri: 
         - prossima_cella : cella di cui si vuole calcolare la possibile collisione
-    Output:
-        - bool : true se avviene collisione, false altrimenti
     */
     bool collisione(const posizione &cella) const;
 };
+
 #endif
